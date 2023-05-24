0534befa6fb8a42cb8a528f577d04c9f37e9c2e3
enot <enotus.chang@gmail.com>
2023-05-24 13:04:06 +0500

    Добавил роуты

--- src/components/AddCard.vue
+++ src/components/AddCard.vue
@@ -1,8 +1,9 @@
 <script setup lang="ts">
+
 </script>
 
 <template>
-  <router-view></router-view>
+  AddCard
 </template>
 
 <style scoped>
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,26 @@
 import { createApp } from 'vue'
+import { createRouter, createWebHistory } from 'vue-router'
 import './style.css'
 import App from './App.vue'
+import Dashboard from './components/Dashboard.vue'
+import AddCard from './components/AddCard.vue'
 
-createApp(App).mount('#app')
+const routes = [
+  {
+    name: 'dashboard',
+    path: '/',
+    component: Dashboard,
+  },
+  {
+    name: 'add-card',
+    path: '/add-card',
+    component: AddCard,
+  },
+]
+const router = createRouter({
+  history: createWebHistory(),
+  routes,
+})
+const app = createApp(App)
+app.use(router)
+app.mount('#app')
